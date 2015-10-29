b0b1437aba824d42008216596a2dc11b7050ce17
viorelcojocaru <viorelcojocaru2008@gmail.com>
2015-10-29 16:30:26 +0200

    renamed for run program

--- src/test/java/dao/TestUser.java
+++ src/test/java/dao/TestUser.java
@@ -8,61 +8,54 @@ package dao;
 import java.util.ArrayList;
 import java.util.List;
 import model.User;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  *
  * @author Olga
  */
-public class TestUser {    
-   
+public class TestUser {
 
+    @Test
     private void saveUserTest(User user) {
         UserIntf userBC = UserImpl.getInstance();
         assert (user != null);
         userBC.saveUser(user);
     }
 
+    @Test
     private void updateUserTest(User user) {
         UserIntf userBC = UserImpl.getInstance();
         assert (user != null);
         userBC.updateUser(user);
     }
 
+    @Test
     private void deleteUserTest(User user) {
         UserIntf userBC = UserImpl.getInstance();
         assert (user != null);
         userBC.deleteUser(user);
     }
-    
-    private User getUserByIdTest(int id) {        
+
+    @Test
+    private User getUserByIdTest(int id) {
         UserIntf userBC = UserImpl.getInstance();
         User user = new User();
         user = userBC.getUserById(id);
-        assert (user!=null);        
+        assert (user != null);
+        Assert.assertEquals("balishoi", user.getLogin());
         return user;
     }
-    
-  public List<User> getUserListTest(){
+
+    @Test
+    public List<User> getUserListTest() {
         UserIntf userBC = UserImpl.getInstance();
-        List<User> list=new ArrayList<User>();
-        list=userBC.getUserList();
-        assert(list!=null);
-        assert(!list.isEmpty()); 
+        List<User> list = new ArrayList<User>();
+        list = userBC.getUserList();
+        assert (list != null);
+        assert (!list.isEmpty());
         return null;
     }
-    
-    public static void main(String[] args) {
-      TestUser test=new TestUser();
-      /*
-      if (user != null) {
-            System.out.println(user.getId());
-            System.out.println(user.getLogin());
-            System.out.println(user.getPassword());
-            System.out.println(user.getRedirectUrl());
-        } else {
-            System.out.println("no such Ads with id=" + id);
-        }*/ 
-           
-    }
-            
+
 }
