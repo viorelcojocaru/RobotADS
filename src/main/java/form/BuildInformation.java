8088fe3da74f2ad8ec05dfe33009d3d18f10344b
viorelcojocaru <viorelcojocaru2008@gmail.com>
2015-11-06 20:23:51 +0200

    renamed for run program

--- src/main/java/form/BuildInformation.java
+++ src/main/java/form/BuildInformation.java
@@ -17,130 +17,116 @@ import dao.AdsIntf;
 import dao.UserImpl;
 import dao.UserIntf;
 
-public class BuildInformation implements Runnable{
-	String sleep;
-	public BuildInformation(String sleep) {
-		this.sleep=sleep;
-	}
-	private Advertisement getAdsXml() {		
-		Advertisement adsxml = null;
-		ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"Beans.xml");		
-		adsxml = new Advertisement();
-		adsxml = (Advertisement) ctx.getBean("ads");
-		ctx.close();
-		return adsxml;
-	}
-	
-	private   List<Advertisement> getAdvertisementList() {
-		Advertisement adsXml = getAdsXml();
-		AdsIntf adsBc = AdsImpl.getInstance();
-		List<Advertisement> advertismentMapList = new ArrayList<Advertisement>();
-		List<Ads> bdAdsList = new ArrayList<Ads>();
-		bdAdsList = adsBc.getAdsList();
-		
-		Map<String, String> tempInputAdsForm;
-		Map<String, String> tempLinksAdsMap=adsXml.getLinksAdsMap();
-		List<String> tempStepsLinks=adsXml.getStepsLinks();
-		
-		if (bdAdsList != null && !bdAdsList.isEmpty()) {
-			for (Ads oneAds : bdAdsList) {
-				tempInputAdsForm=new HashMap<String, String>();//= adsXml.getInputAdsForm();
-				tempInputAdsForm.putAll(adsXml.getInputAdsForm());
-				Advertisement a = new Advertisement();				
-				tempInputAdsForm.put("postAdForm.title", oneAds.getTitle());
-				tempInputAdsForm.put("postAdForm.youtubeVideoURL", oneAds.getYoutubeVideoUrl());
-				tempInputAdsForm.put("postAdForm.phoneNumber", oneAds.getPhoneNumber());
-				tempInputAdsForm.put("postAdForm.mapAddress", oneAds.getMapAddress());
-				tempInputAdsForm.put("postAdForm.description", oneAds.getDescription());
-				tempInputAdsForm.put("file", oneAds.getImagePath());				
-				a.setInputAdsForm(tempInputAdsForm);
-				a.setLinksAdsMap(tempLinksAdsMap);
-				a.setStepsLinks(tempStepsLinks);
-				advertismentMapList.add(a);
-			}
-		}
-		return advertismentMapList;
-	}
-    
-        private User getUser(){
-            UserIntf userBc= UserImpl.getInstance();
-           User user= userBc.getUserById(1);
-           return user;
+public class BuildInformation implements Runnable {
+
+    String sleep;
+
+    public BuildInformation(String sleep) {
+        this.sleep = sleep;
+    }
+
+    private Advertisement getAdsXml() {
+        Advertisement adsxml = null;
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "Beans.xml");
+        adsxml = new Advertisement();
+        adsxml = (Advertisement) ctx.getBean("ads");
+        ctx.close();
+        return adsxml;
+    }
+
+    private List<Advertisement> getAdvertisementList() {
+        Advertisement adsXml = getAdsXml();
+        AdsIntf adsBc = AdsImpl.getInstance();
+        List<Advertisement> advertismentMapList = new ArrayList<Advertisement>();
+        List<Ads> bdAdsList = new ArrayList<Ads>();
+        try{
+            bdAdsList = adsBc.getAdsList();
+        }catch(Exception ex){
+            ex.printStackTrace();
         }
+        Map<String, String> tempInputAdsForm;
+        Map<String, String> tempLinksAdsMap = adsXml.getLinksAdsMap();
+        List<String> tempStepsLinks = adsXml.getStepsLinks();
 
-	void postAds() {
-		LoginDeletePostSingOutKijiji loginKijiji = new LoginDeletePostSingOutKijiji();
-		List<Advertisement> listaXml = getAdvertisementList();
+        if (bdAdsList != null && !bdAdsList.isEmpty()) {
+            for (Ads oneAds : bdAdsList) {
+                tempInputAdsForm = new HashMap<String, String>();//= adsXml.getInputAdsForm();
+                tempInputAdsForm.putAll(adsXml.getInputAdsForm());
+                Advertisement a = new Advertisement();
+                tempInputAdsForm.put("postAdForm.title", oneAds.getTitle());
+                tempInputAdsForm.put("postAdForm.youtubeVideoURL", oneAds.getYoutubeVideoUrl());
+                tempInputAdsForm.put("postAdForm.phoneNumber", oneAds.getPhoneNumber());
+                tempInputAdsForm.put("postAdForm.mapAddress", oneAds.getMapAddress());
+                tempInputAdsForm.put("postAdForm.description", oneAds.getDescription());
+                tempInputAdsForm.put("file", oneAds.getImagePath());
+                a.setInputAdsForm(tempInputAdsForm);
+                a.setLinksAdsMap(tempLinksAdsMap);
+                a.setStepsLinks(tempStepsLinks);
+                advertismentMapList.add(a);
+            }
+        }
+        return advertismentMapList;
+    }
 
-		int i = 0;
-		int j = 0;
-		while (i < 10) {
-			if (j == listaXml.size()) {
-				j = 0;
-			}
-			Advertisement oneAdsXml = listaXml.get(j);
-			j++;
-			try {
-				loginKijiji.setAdsKijiji(oneAdsXml, getUser());
-				loginKijiji.loginInAccount();
-				long random1 = (long) (Math.random() * (2 - 10) + 10);
-				Thread.sleep(random1 * 1000L);
-				
-				loginKijiji.deleteAds();
-				long random2 = (long) (Math.random() * (2 - 10) + 10);
-				Thread.sleep(random2 * 1000L);
-				
-				loginKijiji.postAds();
-				++i;
-				Thread.sleep(stringMinToMilisecond(sleep));
+    private User getUser() {
+        UserIntf userBc;
+        User user = null;
+        try {
+            userBc = UserImpl.getInstance();
+            user = userBc.getUserById(1);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return user;
+    }
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    void postAds() {
+        LoginDeletePostSingOutKijiji loginKijiji = new LoginDeletePostSingOutKijiji();
+        List<Advertisement> listaXml = getAdvertisementList();
 
-	private long stringMinToMilisecond(String strMin) {
-		long intMin = Long.parseLong(strMin);
-		long milisecond = 1000;
-		if (intMin <= 0) {
-			intMin = 1;
-		} else {
-			milisecond = intMin * 60000;
-		}
-		return milisecond;
-	}
+        int i = 0;
+        int j = 0;
+        while (i < 10) {
+            if (j == listaXml.size()) {
+                j = 0;
+            }
+            Advertisement oneAdsXml = listaXml.get(j);
+            j++;
+            try {
+                if (getUser() != null) {
+                    loginKijiji.setAdsKijiji(oneAdsXml, getUser());
+                    loginKijiji.loginInAccount();
 
-	public static void main(String[] args) {
-//		User user = new User();
-//		UserIntf userBc = UserImpl.getInstance();
-//		user = userBc.getUserById(1);
-//		List<Advertisement> list=new ArrayList<Advertisement>();
-		//list.addAll(getAdvertisementList);
-//		LoginDeletePostSingOutKijiji loginKijiji = new LoginDeletePostSingOutKijiji();
-//		int i = 0;
-//		while (i < 10) {
-//			try {
-////				loginKijiji.getAdsKijiji(ads, user);
-////				loginKijiji.run();
-//                            System.out.println("delete");
-//                            Thread thread = new Thread();
-//				thread.sleep(2000L);
-//                            System.out.println("post");
-//				++i;
-//				
-//				thread.sleep(5000L);
-//
-//			} catch (Exception e) {
-//				// TODO: handle exception
-//			}
-//		}
-		
-		
-	}
+                    long random1 = (long) (Math.random() * (2 - 10) + 10);
+                    Thread.sleep(random1 * 1000L);
+
+                    loginKijiji.deleteAds();
+                    long random2 = (long) (Math.random() * (2 - 10) + 10);
+                    Thread.sleep(random2 * 1000L);
+
+                    loginKijiji.postAds();
+                    ++i;
+                    Thread.sleep(stringMinToMilisecond(sleep));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private long stringMinToMilisecond(String strMin) {
+        long intMin = Long.parseLong(strMin);
+        long milisecond = 1000;
+        if (intMin <= 0) {
+            intMin = 1;
+        } else {
+            milisecond = intMin * 60000;
+        }
+        return milisecond;
+    }
 
     public void run() {
-        postAds(); 
+        postAds();
     }
 }
