c80a6d81b3f35d49bb26ff0e1d841930dac36ef7
viorelcojocaru <viorelcojocaru2008@gmail.com>
2015-11-07 22:55:21 +0200

    renamed for run program

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,18 +1,26 @@
 package model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user")
 public class User implements Serializable {
 
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-
+    @Id@Column (name="id")
     private long id = 1L;
-
+    @Column(name="userUrl")
     private String redirectUrl;
+    @Column (name="name")
     private String login;
+    @Column (name="password")
     private String password;
 
     public String getRedirectUrl() {
