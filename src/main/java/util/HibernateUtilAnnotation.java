38bac4746ea2c9e24b63f032f4751c41927d95b7
viorelcojocaru <viorelcojocaru2008@gmail.com>
2015-11-07 22:52:57 +0200

    renamed for run program

---
+++ src/main/java/util/HibernateUtilAnnotation.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ *
+ * @author Olga
+ */
+public class HibernateUtilAnnotation {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        AnnotationConfiguration cfg = new AnnotationConfiguration();
+        cfg.configure("hibernate.cfg.xml");
+        try {
+            sessionFactory = cfg.buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
